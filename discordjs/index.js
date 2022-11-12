dc2642f91b01d02b669c8707998b0d6e698f9438
tokkyo13 <the.user.is.tokkyo13@gmail.com>
2022-11-13 02:18:52 +0900

    chore: format code by prettier

--- discordjs/index.js
+++ discordjs/index.js
@@ -1,21 +1,22 @@
-const TOKEN = process.env.TOKEN
-const GUILD_ID = process.env.GUILD_ID
-const CHANNEL_ID = process.env.CHANNEL_ID
+const TOKEN = process.env.TOKEN;
+const GUILD_ID = process.env.GUILD_ID;
+const CHANNEL_ID = process.env.CHANNEL_ID;
 
-const { Client, GatewayIntentBits } = require('discord.js');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
+const { Client, GatewayIntentBits } = require("discord.js");
+const {
+    joinVoiceChannel,
+    createAudioPlayer,
+    createAudioResource,
+    AudioPlayerStatus,
+} = require("@discordjs/voice");
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildVoiceStates
-    ]
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
 });
 const player = createAudioPlayer();
-const resource = createAudioResource('./voice.wav');
+const resource = createAudioResource("./voice.wav");
 
-
-client.on('ready', () => {
+client.on("ready", () => {
     const guild = client.guilds.cache.get(GUILD_ID);
     const channel = guild.channels.cache.get(CHANNEL_ID);
 
@@ -24,16 +25,13 @@ client.on('ready', () => {
         guildId: channel.guild.id,
         adapterCreator: channel.guild.voiceAdapterCreator,
     });
-    
 
     player.play(resource);
     connection.subscribe(player);
 });
 
-
 player.on(AudioPlayerStatus.Idle, () => {
     client.destroy();
 });
-
 
 client.login(TOKEN);
