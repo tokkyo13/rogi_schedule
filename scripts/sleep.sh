4d8fec61bcc94a49d4f35a36624179966134524a
tokkyo13 <the.user.is.tokkyo13@gmail.com>
2022-11-14 00:07:35 +0900

    chore: test action in JST

--- scripts/sleep.sh
+++ scripts/sleep.sh
@@ -1,5 +1,5 @@
 current_epoch=$(date +%s)
-target_epoch=$(date -d "tomorrow 00:00" +%s)
+target_epoch=$(TZ=JST-9 date -d "tomorrow 00:15" +%s)
 
 sleep_seconds=$(( $target_epoch - $current_epoch ))
 
